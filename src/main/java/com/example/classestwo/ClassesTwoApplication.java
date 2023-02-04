52838f77e540728d90b69fe58ce06369ac268c05
My Name is Universe <alyonushkagirl22@mail.ru>
2023-02-04 11:28:00 +0300

    Initial commit

---
+++ src/main/java/com/example/classestwo/ClassesTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.classestwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassesTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassesTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/classestwo/ClassesTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classestwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassesTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
